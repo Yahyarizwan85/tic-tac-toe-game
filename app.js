b276f7c044f82699ebf6d3558a856242ae203529
YahyaRizwan <155488461+Yahyarizwan85@users.noreply.github.com>
2025-07-04 13:06:55 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,58 @@
+let boxes = document.querySelectorAll(".box")
+let msg = document.querySelector("#msg")
+let resetBtn = document.querySelector(".reset-btn")
+
+let turn0 = true
+const winPattern = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+]
+const disabledBoxes = () =>{
+    for(let box of boxes ){
+        box.disabled = true;
+    }
+}
+
+boxes.forEach((box) => {
+    box.addEventListener("click", () =>{
+        console.log(`${box} was clicked`)
+        if (turn0){
+            box.style="color:blue"
+            box.innerText = "O";
+            turn0 = false;
+        }
+        else{
+            box.innerText = "X";
+            turn0 = true
+        }
+        box.disabled = true;
+        
+        checkWinner()
+        
+    })
+    
+});
+
+const checkWinner = ()=>{
+    for(let pattern of winPattern){
+        let pos1val = boxes[pattern[0]].innerText
+        let pos2val = boxes[pattern[1]].innerText
+        let pos3val = boxes[pattern[2]].innerText
+        if(pos1val != "" && pos2val != "" && pos3val != ""){
+
+            if (pos1val === pos2val && pos2val === pos3val){
+                msg.innerText = `Congratulations ${pos1val} is winner`
+                disabledBoxes()
+                
+            }
+        }
+    }
+
+
+}
